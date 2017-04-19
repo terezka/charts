fb0676a8630f10f7e17739918bd3ce7ec6cca131
Tereza Sokol <terezasokol@gmail.com>
2017-04-19 12:17:54 +0200

    Update docs

--- src/Svg/HeatMap.elm
+++ src/Svg/HeatMap.elm
@@ -1,6 +1,6 @@
 module Svg.HeatMap exposing (HeatMap, Tile, ColorScale(..), heatmap)
 
-{-| _Disclaimer:_ If you're looking to a plotting library, then
+{-| _Disclaimer:_ If you're looking for a plotting library, then please
 use [elm-plot](https://github.com/terezka/elm-plot) instead, as this library is not
 made to be user friendly. If you feel like you're missing something in elm-plot,
 you're welcome to open an issue in the repo and I'll see what I can do
