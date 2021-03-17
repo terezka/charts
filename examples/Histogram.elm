3f83ec80aaaa58e6722e222da0f69181c415fd9a
Tereza Sokol <terezasokol@gmail.com>
2021-03-17 10:43:59 +0100

    Clean up example

--- examples/Histogram.elm
+++ examples/Histogram.elm
@@ -65,6 +65,7 @@ view model =
     , viewHover model
     ]
 
+
 viewBasic : H.Html Msg
 viewBasic =
   C.chart
@@ -93,7 +94,6 @@ viewBasic =
     ]
 
 
-
 viewHover : Model -> H.Html Msg
 viewHover model =
   C.chart
@@ -110,11 +110,7 @@ viewHover model =
     , C.events
         [ C.event "mouseleave" (C.map (\_ -> OnHover []) C.getCoords)
         , C.event "mousemove" <|
-            C.map OnHover
-              --(C.noUnknowns C.getBars >> C.getNearestX)
-              --(C.noUnknowns C.getDots >> C.getWithin 20)
-              (C.getNearestX (C.withoutUnknowns >> C.getBars))
-              --(C.getNearest (C.withoutUnknowns >> C.getDots))
+            C.map OnHover (C.getNearestX (C.withoutUnknowns >> C.getBars))
         ]
     ]
     [ C.grid []
@@ -123,8 +119,6 @@ viewHover model =
         [ C.tickLength 4
         , C.spacing 0
         , C.margin 0
-        --, C.binWidth (always 2)
-        --, C.binLabel .label
         ]
         [ C.bar .z
             [ C.barColor (\d -> C.pink)
@@ -132,7 +126,6 @@ viewHover model =
             , C.unit "m2"
             , C.topLabel (.z >> Maybe.map String.fromFloat)
             ]
-        --, C.bar .y [ C.barColor (\d -> C.blue), C.label "speed", C.unit "ms" ]
         ]
         data
 
@@ -140,26 +133,11 @@ viewHover model =
     , C.yTicks []
     , C.xAxis []
     , C.yLabels []
-    --, C.xTicks [ C.amount 10 ]
     , C.xLabels []
-
-    --, C.series .x
-    --    [ C.linear .y [ C.color C.blue ]
-    --    , C.linear .z []
-    --    ]
-    --    data
-
-    --, C.when model.hoveringDots <| \item rest ->
-    --    C.tooltipOnTop (always item.position.x1) (always item.position.y) []
-    --      [ tooltipRow item ]
 
     , C.when model.hovering <| \item rest ->
         C.tooltipOnTop (always item.center.x) (always item.position.y) []
           [ tooltipRow item ]
-
-    --, C.when model.hovering <| \group rest ->
-    --    C.tooltipOnTop (\_ -> group.position.x) (\_ -> group.position.y) [] <|
-    --      List.map tooltipRow group.bars
     ]
 
 
