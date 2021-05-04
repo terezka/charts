23e2d7a959534aef68912776935379b94f128408
Tereza Sokol <terezasokol@gmail.com>
2021-05-04 11:27:05 +0200

    Adjust display data for line chart

--- examples/src/Section/LineChart.elm
+++ examples/src/Section/LineChart.elm
@@ -219,15 +219,15 @@ data =
   let toDatum x y z v w p q =
         Datum x (Just y) (Just z) (Just v) (Just w) (Just p) (Just q)
   in
-  [ toDatum 0.0 2.0 4.0 4.6 6.9 7.3 8.0
-  , toDatum 0.2 3.0 4.2 5.2 6.2 7.0 8.7
-  , toDatum 0.8 4.0 4.6 5.5 5.2 7.2 8.1
-  , toDatum 1.0 2.0 4.2 5.3 5.7 6.2 7.8
-  , toDatum 1.2 5.0 3.5 4.9 5.9 6.7 8.2
-  , toDatum 2.0 2.0 3.2 4.8 5.4 7.2 8.3
-  , toDatum 2.3 1.0 4.3 5.3 5.1 7.8 7.1
-  , toDatum 2.8 3.0 2.9 5.4 3.9 7.6 8.5
-  , toDatum 3.0 2.0 3.6 5.8 4.6 6.5 6.9
-  , toDatum 4.0 1.0 4.2 4.5 5.3 6.3 7.0
+  [ toDatum 1  2 1 4.6 6.9 7.3 8.0
+  , toDatum 2  3 2 5.2 6.2 7.0 8.7
+  , toDatum 3  4 3 5.5 5.2 7.2 8.1
+  , toDatum 4  3 4 5.3 5.7 6.2 7.8
+  , toDatum 5  2 3 4.9 5.9 6.7 8.2
+  , toDatum 6  4 1 4.8 5.4 7.2 8.3
+  , toDatum 7  5 2 5.3 5.1 7.8 7.1
+  , toDatum 8  6 3 5.4 3.9 7.6 8.5
+  , toDatum 9  5 4 5.8 4.6 6.5 6.9
+  , toDatum 10 4 3 4.5 5.3 6.3 7.0
   ]
 
